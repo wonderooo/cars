644ef4dba64de7e47d5b16d7c02b886b599b6bed
kacpers <kacperek.szczesny@gmail.com>
2025-11-08 02:36:01 +0100

    feat: move sensitive data from yaml config to .env file

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,14 +1,34 @@
+use dotenvy::dotenv;
 use serde::Deserialize;
 use std::sync::LazyLock;
 
 pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    dotenv().ok();
+
     let path = std::env::var("CONFIG_PATH").unwrap_or_else(|_| "config.yaml".into());
     let config_file = std::fs::read_to_string(path).expect("failed to open config file");
-    serde_yaml::from_str(&config_file).expect("failed to parse config file")
+    let mut unmarshalled =
+        serde_yaml::from_str::<Config>(&config_file).expect("failed to parse config file");
+
+    let copart_user = std::env::var("COPART_USER").expect("COPART_USER env var not set");
+    let copart_password =
+        std::env::var("COPART_PASSWORD").expect("COPART_PASSWORD env var not set");
+    let data_bright_user =
+        std::env::var("DATA_BRIGHT_USER").expect("DATA_BRIGHT_USER env var not set");
+    let data_bright_password =
+        std::env::var("DATA_BRIGHT_PASSWORD").expect("DATA_BRIGHT_PASSWORD env var not set");
+
+    unmarshalled.copart.user = copart_user;
+    unmarshalled.copart.password = copart_password;
+    unmarshalled.data_bright.user = data_bright_user;
+    unmarshalled.data_bright.password = data_bright_password;
+
+    unmarshalled
 });
 
 #[derive(Deserialize)]
 pub struct Config {
+    #[serde(skip)]
     pub copart: Copart,
     pub proxy: Proxy,
     pub minio: Minio,
@@ -18,7 +38,8 @@ pub struct Config {
     pub data_bright: DataBright,
 }
 
-#[derive(Deserialize)]
+// Default impl for serde to skip copart field
+#[derive(Default)]
 pub struct Copart {
     pub user: String,
     pub password: String,
@@ -60,7 +81,9 @@ pub struct Loki {
 pub struct DataBright {
     pub host: String,
     pub port: u16,
+    #[serde(skip)]
     pub user: String,
+    #[serde(skip)]
     pub password: String,
     pub allow_domains: Vec<String>,
 }
